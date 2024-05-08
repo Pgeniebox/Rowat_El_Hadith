72f66c04d6595de81aa4e213d35c9dad4a34d7aa
Your Name <you@example.com>
2024-05-08 16:32:12 +0100

    gfg

--- windowManager.js
+++ windowManager.js
@@ -7,9 +7,7 @@ var isEventHandled = false;
 var windmove = null;
 var offsetX, offsetY, initialX, initialY, initialWidth, initialHeight;
 document.addEventListener('mousemove', function(e) {
-    switch (e.type) {
-        case 'mousemove':
-            if (windmove !== null) {
+            if (null!==windmove) {
                 if (windmove.state === 'move') {
                     windmove.style.boxShadow = 'var(--shadow)';
                     windmove.style.left = e.clientX - offsetX + 'px';
@@ -23,12 +21,11 @@ document.addEventListener('mousemove', function(e) {
                     windmove.style.height = newHeight + 'px';
                 }
             } else {
-                if (null != e.target.attributes.title) {
-                    requestAnimationFrame(()=>{toolti(e,e.target)});
+                if (undefined !== e.target.attributes.title) {
+                    queueMicrotask(()=>{toolti(e,e.target)});
+                    $(this).clearQueue();
                 }
             }
-            return;
-    }
 });
 
 document.addEventListener('mousedown', function(e) {
@@ -50,51 +47,50 @@ document.addEventListener('mousedown', function(e) {
                     initialHeight = windmove.offsetHeight;
                     break;
             }
-            return;
+            $(this).clearQueue();
+
     }
 });
 
 document.addEventListener('mouseup', function(e) {
-    switch (e.type) {
-        case 'mouseup':
-            switch(e.target.slot){
-                case 'min':
-                    const wind = e.target.parentElement.parentElement;
-                    wind.hidden = true;
-                    const newT = document.createElement('p');
-                    document.getElementById('taskBar').appendChild(newT);
-                    newT.style.border = '1px solid var(--border-color)';
-                    newT.style.borderRadius = '5px';
-                    newT.style.padding = '5px';
-                    newT.classList = 'newT';
-                    newT.style.pointerEvents = 'all';
-                    newT.style.cursor = 'pointer';
-                    newT.slot = 'open';
-                    newT.style.zIndex = '1';
-                    newT.style.fontWeight = '600';
-                    newT.style.position = 'relative';
-                    newT.onclick = function (){    document.querySelector('[name="' + this.textContent + '"]').hidden = false;
-                    this.remove();
-
-                    }
-                    newT.textContent = wind.attributes.name.nodeValue;
-                    break;
-              
-            }        
             if(null!=windmove){windmove.style.boxShadow = '';   windmove=null;}
-            return;
-    }
+            $(this).clearQueue();
 });
 
-document.addEventListener('mouseout', function(e) {
-    switch (e.type) {
-        case 'mouseout':
-            if(!toolt.hidden){toolt.style.opacity = '0'}    
-            break; 
-    }
+document.addEventListener('mouseout', function() {
+            if(!toolt.hidden){toolt.style.opacity = '0'}
+            $(this).clearQueue();  
 });
 
-
+$('svg').on('click',function(){
+    switch(this.slot){
+        case 'min':
+            const wind = this.parentElement.parentElement;
+            wind.hidden = true;
+            const newT = document.createElement('p');
+            document.getElementById('taskBar').appendChild(newT);
+            newT.style.border = '1px solid var(--border-color)';
+            newT.style.borderRadius = '5px';
+            newT.style.padding = '5px';
+            newT.classList = 'newT';
+            newT.style.pointerEvents = 'all';
+            newT.style.cursor = 'pointer';
+            newT.slot = 'open';
+            newT.style.zIndex = '1';
+            newT.style.fontWeight = '600';
+            newT.style.position = 'relative';
+            newT.onclick = function (){    document.querySelector('[name="' + this.textContent + '"]').hidden = false;
+            this.remove();
+            }
+            newT.textContent = wind.attributes.name.nodeValue;
+            break;
+      case 'close':
+        toolt.hidden = true;
+        toolt.style.opacity = '0';
+        this.parentElement.parentElement.remove();
+        break;
+    }        
+});
 
 function toolti(e,a){
     toolt.textContent = a.attributes.title.value;
